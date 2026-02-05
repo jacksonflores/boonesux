73ea3eda6f1238dbf3277790ef77dee5b3501ae0
jackson flores <jacksonononon@gmail.com>
2026-02-04 21:01:22 -0500

    healthcheck route

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -5,7 +5,7 @@ import "net/http"
 func (a *App) Routes() http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/health", a.healthz)
+	mux.HandleFunc("/healthz", a.healthz)
 
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static"))))
 
