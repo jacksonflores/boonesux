4be38144ed108d1c433bd77c401028f97f5f7041
jackson flores <jacksonononon@gmail.com>
2026-02-10 20:51:39 -0500

    test fonts

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -8,3 +8,10 @@ func (a *App) healthz(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "err", http.StatusServiceUnavailable)
 	}
 }
+
+func (a *App) about(w http.ResponseWriter, r *http.Request) {
+	err := a.Templates.ExecuteTemplate(w, "ramblings.html", nil)
+	if err != nil {
+		http.Error(w, "err", http.StatusInternalServerError)
+	}
+}
--- internal/app/routes.go
+++ internal/app/routes.go
@@ -6,6 +6,7 @@ func (a *App) Routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/healthz", a.healthz)
+	mux.HandleFunc("/", a.about)
 
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static"))))
 
